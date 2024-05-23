f7fafc1e9b501588c3bb69db1afd718f89bf8153
Savic <Momcilo.Savic@FISGLOBAL.COM>
2024-05-23 22:37:05 +0200

    remove unnecesarry

--- dotnet/Utils/EmailStatus.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace EmailOperator.Utils{
-    public class EmailStatus {
-        public string DeliveryStatus { get; set; }
-        public string MessageId { get; set; }
-        public string Error { get; set; }
-    }
-}
