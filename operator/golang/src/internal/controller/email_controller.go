d7c1b1d54942db9f29554b5d0743436d842ec938
Savic <Momcilo.Savic@FISGLOBAL.COM>
2024-05-26 18:14:29 +0200

    .

---
+++ operator/golang/src/internal/controller/email_controller.go
@@ -0,0 +1,140 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"bytes"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+    "k8s.io/apimachinery/pkg/api/errors"
+    corev1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	mailerlitecomv1 "github.com/momchilosavic/mailerlite/api/v1"
+)
+
+// EmailReconciler reconciles a Email object
+type EmailReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=mailerlite.com,resources=emails,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=mailerlite.com,resources=emails/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=mailerlite.com,resources=emails/finalizers,verbs=update
+//+kubebuilder:rbac:groups=v1,resources=secrets,verbs=get
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Email object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
+func (r *EmailReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	err := error(nil)
+
+	email := &mailerlitecomv1.Email{}
+	err = r.Get(ctx, req.NamespacedName, email)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+	
+	// skip if not newly created
+	if email.Status.DeliveryStatus != "" {
+		return ctrl.Result{}, nil
+	}
+	
+	config := &mailerlitecomv1.EmailSenderConfig{}
+	err = r.Get(ctx, client.ObjectKey{Namespace: req.NamespacedName.Namespace, Name: email.Spec.SenderConfigRef}, config)
+	if err != nil {
+		email.Status = mailerlitecomv1.EmailStatus{DeliveryStatus: "Failed", Error: "Sender config not found"}
+		_ = r.Status().Update(ctx, email)
+		return ctrl.Result{}, err
+	}
+	
+	secret := &corev1.Secret{}
+	err = r.Get(ctx, client.ObjectKey{Namespace: req.Namespace, Name: config.Spec.ApiTokenSecretRef}, secret)
+	if err != nil {
+		email.Status = mailerlitecomv1.EmailStatus{DeliveryStatus: "Failed", Error: "Api Token Secret not found"}
+		_ = r.Status().Update(ctx, email)
+		return ctrl.Result{}, err
+	}
+	
+	apiToken := string(secret.Data["token"])
+	url := "https://api.mailersend.com/v1/email"
+	
+	requestBody, err := json.Marshal(map[string]interface{} {
+		"from": map[string]string {
+			"email": config.Spec.SenderEmail,
+		},
+		"to": []map[string]string {
+			{ "email": email.Spec.RecipientEmail },
+		},
+		"subject": email.Spec.Subject,
+		"body": email.Spec.Body,
+	})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	
+	httpReq, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	httpReq.Header.Set("Content-Type", "application/json")
+	httpReq.Header.Set("X-MailerLite-ApiKey", apiToken)
+	
+	client := &http.Client{}
+	
+	logger.Info("\n\nHERE\n\n")
+	
+	httpResp, err := client.Do(httpReq)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	logger.Info("\n\nHERE2\n\n")
+	defer httpResp.Body.Close()
+	logger.Info("\n\nHERE3\n\n")
+	if httpResp.StatusCode != http.StatusOK {
+	logger.Info("\n\nHERE4\n\n")
+		return ctrl.Result{}, fmt.Errorf("Failed to send email")
+	}
+	logger.Info("\n\nHERE5\n\n")
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *EmailReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&mailerlitecomv1.Email{}).
+		Complete(r)
+}
